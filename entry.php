82f57d5ebeb771c14b96d1434f3dd2452d3b9daa
zhaocx <chunxia_zhao@yahoo.com>
2014-11-30 13:06:17 -0800

    made changes

---
+++ entry.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Entry</title>
+</head>
+
+<body>
+<?php include("dbconnect.php"); 
+//recover the form variable values.
+$title= mysql_real_escape_string(trim(strip_tags($_POST['title'])),$dbc);
+$link= mysql_real_escape_string(trim(strip_tags($_POST['link'])),$dbc);
+$length= $_POST['length'];
+$resolution= mysql_real_escape_string(trim(strip_tags($_POST['resolution'])),$dbc);
+$desc= mysql_real_escape_string(trim(strip_tags($_POST['desc'])),$dbc);
+$language = mysql_real_escape_string(trim(strip_tags($_POST['language'])),$dbc);
+$count= $_POST['count'];
+$type = "";
+if(!empty($_POST['type']))
+	$type= implode(',', $_POST['type']);
+$image =mysql_real_escape_string(trim(strip_tags($_POST['image'])),$dbc);
+$tag = mysql_real_escape_string(trim(strip_tags($_POST['tag'])),$dbc); 
+
+$query = "insert into fun_video "
+        ." (title,videolink,videolength,highestresolution,description,language,viewcount,videotype,iconimage,tag) values "
+                ."('$title', '$link', '$length', '$resolution', '$desc','$language','$count','$type','$image','$tag')"
+        ;
+
+//var_dump($query);	   
+if(!empty($link) || !empty($title) || !empty($image)){
+	mysqli_query($conn,$query);
+	print "<h2>Thanks!!</h2>";
+}
+else 
+	print "<h2>Your input is incomplete and has not be added!</h2>";
+?>
+<h2><a href="viewData.php">View My Video Collection!!!</a></h2>
+</body>
+</html>
