0f161e803cb5f7768c6b5bb78a8534a8b0dc3ff3
zhaocx <chunxia_zhao@yahoo.com>
2014-11-30 23:38:30 -0800

    added new

--- welcome.php
+++ welcome.php
@@ -1,3 +1,6 @@
+<?php
+session_start();
+?>
 <!doctype html>
 <html>
 <head>
@@ -9,12 +12,18 @@
 <?php 
 //define('TITLE', 'welcome');
 //include ('header.html');
-?>
 
-<h1>Welcome to my site</h1>
+print '<h1>Welcome to my site</h1>';
+//print '<p>Hello, ' . $_SESSION['email'] . '!</p>';
+?>
 <p><a href="enterData.php">Add new video!</a></p>
 
-<a href="viewData.php">Browse my video collection!</a>
-
+<p><a href="viewData.php">Browse my video collection!</a></p>
+<div>
+<p>Click <a href="logout.php">here</a> to logout.</p>
+</div>
+<?php
+//include('footer.html');
+?>
 </body>
 </html>
