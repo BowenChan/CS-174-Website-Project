8cd8c53b1a344ec40e377ba7bfdc70e0c941f999
zhaocx <chunxia_zhao@yahoo.com>
2014-11-30 22:53:22 -0800

    added new

---
+++ welcome.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Welcome</title>
+</head>
+
+<body>
+<?php 
+//define('TITLE', 'welcome');
+//include ('header.html');
+?>
+
+<h1>Welcome to my site</h1>
+<p><a href="enterData.php">Add new video!</a></p>
+
+<a href="viewData.php">Browse my video collection!</a>
+
+</body>
+</html>
