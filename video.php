be36f3022f7d0e3474d51b90e8b137f48ab05ae4
BowenChan <BowenmChan@gmail.com>
2014-12-01 18:43:04 -0800

    Added a missing file

---
+++ video.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+<?php
+	include('header.html');
+	include('multiple_sort.php');
+	include('display.php');
+	display('selectdb.php');
+?>
+</body>
+</html>
