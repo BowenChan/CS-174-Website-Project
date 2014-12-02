d293d81d59a0ff49a2fe4183a65c0a81915a955d
BowenChan <BowenmChan@gmail.com>
2014-12-01 18:53:52 -0800

    added favorite video code

---
+++ favoriteVideo.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+<?php
+	session_start();
+	include('connect.php');
+	include('selectdbfav.php');
+	if(isset($_SESSION['id'])){
+		$userID = $_SESSION['sess_user'];
+		$vidID = $_GET['id'];
+		$result = mysqli_query($link, "SELECT * FROM `fav_video` WHERE `user_id`=".$userID." AND `video_id`=".$vidID);
+		if(mysqli_num_rows($result) === 0)
+		{
+			$sql = "INSERT INTO `fav_video`(`user_id`,`video_id`) VALUES ('$userID', '$vidID')";
+			$retval = mysqli_query($link, $sql) or die("</br>Error: " . mysqli_error($link));
+			
+		}
+		echo $_GET['id'];
+		header('Location: video.php');
+	}
+	else{
+		header('Location: login.php');
+	}
+?>
+</body>
+</html>
