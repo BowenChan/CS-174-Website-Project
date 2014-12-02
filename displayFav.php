5eba87eac75cc27d6c38c0e3cf8b9e42a1ceb62d
BowenChan <BowenmChan@gmail.com>
2014-12-01 18:26:01 -0800

    Add fav video and required files

---
+++ displayFav.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+<?php
+	session_start();
+	include('header.html');
+	include('display.php');
+	display('selectdbfav.php');
+	
+?>
+</body>
+</html>
