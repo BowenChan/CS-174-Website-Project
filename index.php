95aac5da677c161969db74ecb9824a33d5ce33d3
Kory Le <kory.le@sjsu.edu>
2014-12-04 22:50:10 -0800

    Setting up twitter bootstrap
    - uploaded bootstrap
    - tested some fonts on index.php

--- index.php
+++ index.php
@@ -1,17 +1,38 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="UTF-8">
-<title>index</title>
-</head>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Team 3</title>
 
-<body>
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Main CSS -->
+    <link href="css/main.css" rel="stylesheet">
+	<!-- Fonts -->
+	<link href="http://fonts.googleapis.com/css?family=Ubuntu:bold" rel="stylesheet" type="text/css">
+	<link href="http://fonts.googleapis.com/css?family=Vollkorn" rel="stylesheet" type="text/css">
 
-<h1 style="color: blue;">Welcome to the world of Team Project 3!</h1>
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <h1>Welcome to the world of Team Project 3!</h1>
 
-<p><a href="login.php">Access my video collection</a></p>
+    <p><a href="login.php">Access my video collection</a></p>
 
-<p><a href="register.php">Register</a></p>
+    <p><a href="register.php">Register</a></p>
 
-</body>
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
 </html>
