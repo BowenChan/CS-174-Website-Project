6520d74ea983079de6ed3b30e6901e42b202c110
BowenChan <BowenmChan@gmail.com>
2014-12-13 18:44:19 -0800

    created a conditional for header
    only show login and register if not logged in.

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
 </head>
 
 <body>
-<?php // session_start() Session already started during login. Don't need to do it again.?>
+<?php 	session_start();// session_start() Session already started during login. Don't need to do it again.?>
 <nav width="100%" height="56" >
   <ul>
    	<li><a href = "welcome.php">Home</a></li>
@@ -60,9 +60,14 @@
     </li>
     <li><a href = "video.php">View All</a></li>
     <li><a href = "modify.php">Modify A Video</a></li>
-    <li><a href = "login.php">Login </a></li>
-    <li><a href = "register.php"> Register </a></li>
-    <li><a href = "displayFav.php"> Display Favorite </a></li>
+    <?php
+	if(!isset($_SESSION['sess_users']))
+	{
+    	echo "<li><a href = 'login.php'>Login </a></li>";
+    	echo "<li><a href = 'register.php'> Register </a></li>";
+	}
+	?>
+	<li><a href = "displayFav.php"> Display Favorite </a></li>
   </ul>
 </nav>
 </body>
