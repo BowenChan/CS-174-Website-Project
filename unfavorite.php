e11af046abc919fabdc06705a937246e7027d5a3
Benton Li <bentonli@comcast.net>
2014-12-18 15:16:45 -0800

    FIXED UNFAVORITE!

--- unfavorite.php
+++ unfavorite.php
@@ -1,17 +1,28 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Untitled Document</title>
-</head>
-
-<body>
-
 <?php
-include('connect.php');
-include('selectdbfav.php');
-
-function unfavoriteCheck() {
+	session_start();
+	ob_start();
+	include('connect.php');
+	include('selectdbfav.php');
+	if(isset($_SESSION['sess_users'])){
+		
+		$userID = $_SESSION['sess_users'];
+		$vidID = $_GET['id'];
+		$result = mysqli_query($link, "SELECT * FROM `fav_video` WHERE `user_id`='".$userID."' AND `video_id`='".$vidID. "'");
+		if(mysqli_num_rows($result) === 1)
+		{
+			
+			$sql = "DELETE FROM fav_video WHERE user_id='".$userID."' AND video_id='".$vidID."'";
+			$retval = mysqli_query($link, $sql);
+			
+		}
+		header('Location: displayFav.php');
+	} else {
+		header('Location: displayFav.php');
+	}
+	header('Location:displayFav.php');
+	
+	
+	function unfavoriteCheck() {
 	
 	if(isset($_SESSION['sess_users'])){
 		
@@ -24,36 +35,19 @@ function unfavoriteCheck() {
 		return true; // IT IS CURRENTLY FAVORITED -- RETURN TRUE TO SHOW UNFAVORITE BUTTON
 	} else return false; // IT IS CURRENTLY NOT FAVORITED -- RETURN FALSE TO SHOW FAVORITE BUTTON
 }
-
-session_start();
-ob_start();
-	include('connect.php');
-	include('selectdbfav.php');
-	if(isset($_SESSION['sess_users'])){
-		
-		$userID = $_SESSION['sess_users'];
-		$vidID = $_GET['id'];
-		$result = mysqli_query($link, "SELECT * FROM `fav_video` WHERE `user_id`='".$userID."' AND `video_id`='".$vidID. "'");
-		if(mysqli_num_rows($result) === 1)
-		{
-			
-			$sql = "DELETE FROM fav_video(user_id,video_id) VALUES ('$userID', '$vidID')";
-			$retval = mysqli_query($link, $sql);
-			
-		}
-		header('Location: video.php');
-	}
-	else{
-		header('Location: video.php');
-	}
-	header('Location:video.php');
-
-
-
-
-
-
-
 ?>
+
+
+
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Unfavorite</title>
+</head>
+
+<body>
+
+
 </body>
 </html>
